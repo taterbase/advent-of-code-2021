c26b6019dbf6bab8d018d4a549ddd163f052feb1
taterbase <shankga@gmail.com>
2021-12-02 01:04:15 -0500

    Day 2 part 1

---
+++ day-2/part-1.rkt
@@ -0,0 +1,26 @@
+#lang racket
+
+(define (to-num str)
+  (string->number (string-trim str)))
+
+(define (move-sub input)
+  
+  (define (iter horiz depth next-line)
+    (if (eof-object? next-line)
+        (begin (close-input-port input)
+               (* horiz depth))
+
+        (let* ([cmd (string-split next-line)]
+              [dir (car cmd)]
+              [amt (to-num (car (cdr cmd)))]
+              [new-line (read-line input)])
+          (cond ((string=? dir "forward")
+                 (iter (+ horiz amt) depth new-line))
+                ((string=? dir "down")
+                 (iter horiz (+ depth amt) new-line))
+                (else
+                 (iter horiz (- depth amt) new-line))))))
+    
+  (iter 0 0 (read-line input)))
+
+(move-sub (open-input-file "input.txt"))
